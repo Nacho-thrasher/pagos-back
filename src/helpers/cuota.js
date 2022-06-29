694805787eb283941bf52fc5e2ee14ccc855c239
ignacio burgos <jiburgos@ucasal.edu.ar>
2022-06-29 15:23:06 -0300

    cuotas/helper

--- src/helpers/cuota.js
+++ src/helpers/cuota.js
@@ -1,9 +1,16 @@
 
 const calcularMontoConInteres = (monto, interes) => {
-
+    if (monto == null || interes == null) return 0.0;
+    const i = interes / 100;
+    const montoConInteres = monto * (1 + i);
+    return montoConInteres;
 }
-const calcularMontoPorCuota = (monto, cuotas) => {
-
+const calcularMontoPorCuota = (monto, cantidad, interes) => {
+    if (monto == null || cantidad == null || cantidad <= 0 || interes == null) 
+    return 0.0;
+    const montoConInteres = calcularMontoConInteres(monto, interes);
+    const montoPorCuota = montoConInteres / cantidad;
+    return montoPorCuota;
 }
 
 module.exports = { calcularMontoConInteres, calcularMontoPorCuota };
